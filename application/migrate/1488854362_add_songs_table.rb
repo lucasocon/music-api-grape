0076124414ce3aa48330049d2bb8786b39281a9d
Lucas Ocon <lucas_masterclas@hotmail.com>
2017-03-07 00:14:02 -0300

    Minor fix on songs table migration

--- application/migrate/1488854362_add_songs_table.rb
+++ application/migrate/1488854362_add_songs_table.rb
@@ -1,9 +1,18 @@
 Sequel.migration do
   change do
-    create_table(:playlists) do
+    create_table(:songs) do
       primary_key :id
       String :name
+      String :genre
+      String :banner
+      String :promotion, text: true
+      Integer :duration
+      Integer :album_id
+      Integer :artist_id
+      FalseClass :featured
 
+      index :album_id
+      index :artist_id
       index :name
     end
   end
